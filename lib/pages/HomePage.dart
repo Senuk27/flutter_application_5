b465330d0e9902c515add2c2b2cd6afc99f81ff3
Senuk thiyasara <96465619+Senuk27@users.noreply.github.com>
2023-07-12 20:16:37 +0530

    first commit

---
+++ lib/pages/HomePage.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_5/main.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //return const HomePage();
+
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text("Senuk"),
+          elevation: 4,
+          backgroundColor: Colors.red),
+      body: const Center(
+        child: Text(
+          "Hi, Welcome to this page",
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold
+
+              // Set the desired font size
+              ),
+        ),
+      ),
+    );
+  }
+}
